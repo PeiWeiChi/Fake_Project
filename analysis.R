31b7a6cebc0399a18a8a0887f0a77df67a1497aa
William C <34178002+PeiWeiChi@users.noreply.github.com>
2019-11-08 16:14:43 +1100

    add plot

--- analysis.R
+++ analysis.R
@@ -1,6 +1,17 @@
 #analysis script for my awesome work
 
-# go
+# read data
+#apples <- data.frame(apple_id = as.factor(1:1000),
+#                     weight=rnorm(1000,5,1))
+
+#write.csv(apples,'data/apples_data.csv')
+# read data
+
+apple <- read.csv('data/apples_data.csv')
+
+png('figs/hist_apple.png')
+hist(apple$weight)
+dev.off()
 
 # gogogo
 
